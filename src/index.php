1fa28668c75f3fd120b488538023e562ba463f49
Sidachenko Michael <michaellux@yandex.ua>
2024-03-09 21:22:43 +0300

    add logic for get all data

--- src/index.php
+++ src/index.php
@@ -21,7 +21,7 @@ $crawler = new Crawler($html);
 $crawler->filter('body');
 
 // loop through the data
-$allNodeValues = [];
+$allLinks = [];
 
 $cardLinkSelector = 'article.listing-item .listing-title > a';
 
@@ -34,16 +34,122 @@ for ($currentPage = $startPage; $currentPage <= $finishPage; $currentPage++) {
   $html = ''.$response->getBody();
 
   $crawler = new Crawler($html);
-  $nodeValues = $crawler->filter($cardLinkSelector)->each(function (Crawler $node, $i): string {
-    // search for values that I want
+  $links = $crawler->filter($cardLinkSelector)->each(function (Crawler $node, $i): string {
     return $node->attr('href');
   });
-  $allNodeValues = array_merge($nodeValues, $allNodeValues);
+  $allLinks = array_merge($links, $allLinks);
 }
 
-if ($expectedQuantityNum == count($allNodeValues))
+//class Car
+class CarInfo {
+    const CONDITION = 'Used';
+    const GOOGLE_PRODUCT_CATEGORY = '123';
+    const STORE_CODE = 'xpremium';
+    const VEHICLE_FULLFILLMENT_OPTION_STORE_CODE = 'in_store:premium';
+
+    private string $brand;
+    private string $model;
+    private string $year;
+    private string $color;
+    private string $mileage;
+    private int $price;
+    private string $VIN;
+    private string $imageLink;
+    private string $linkTemplate;
+
+    public function __construct(
+        string $brand,
+        string $model,
+        string $year,
+        string $color,
+        string $mileage,
+        string $price,
+        string $VIN,
+        string $imageLink,
+        string $linkTemplate
+    ) {
+        $this->brand = $brand;
+        $this->model = $model;
+        $this->year = $year;
+        $this->color = $color;
+        $this->mileage = $mileage;
+        $this->price = (int) str_replace(',', '', $price);
+        $this->VIN = $VIN;
+        $this->imageLink = $imageLink;
+        $this->linkTemplate = $linkTemplate . '?store=' . self::STORE_CODE;
+    }
+}
+
+
+if ($expectedQuantityNum == count($allLinks))
 {
-  print_r($allNodeValues);
+  $carInfos = [];
+  foreach ($allLinks as $key => $link) {
+    $response = $client->request('GET', $link);
+    $html = ''.$response->getBody();
+    echo $link;
+    $crawler = new Crawler($html);
+    $descriptionSelector = '#listing-detail-detail';
+    $description = $crawler->filter($descriptionSelector);
+
+    $priceSelector = 'article[id^="post-"]';
+    $priceBlock = $crawler->filter($priceSelector);
+
+    $brand = $description->filterXPath('//div[contains(text(), "Make")]')->nextAll('.value')->text();
+    
+    $model = $description->filterXPath('//div[contains(text(), "Model")]');
+    if ($model->text('') != '') {
+     $model = $model->nextAll('.value')->text();
+    }
+    else {
+      $model = '';
+    }
+    
+    echo $model;
+    $year = $description->filterXPath('//div[contains(text(), "Year")]');
+    if ($year->text('') != '') {
+      $year = $year->nextAll('.value')->text();
+    }
+    else {
+      $year = '';
+    }
+    echo $year;
+
+    $color = $description->filterXPath('//div[contains(text(), "Color")]');
+    if ($color->text('') != '') {
+      $color = $color->nextAll('.value')->text();
+    }
+    else {
+      $color = '';
+    }
+    echo $color;
+
+    $mileage = $description->filterXPath('//div[contains(text(), "Mileage")]');
+    if ($mileage->text('') != '') {
+      $mileage = $mileage->nextAll('.value')->text();
+    }
+    else {
+      $mileage = '';
+    }
+    echo $mileage;
+
+    $price = $priceBlock->filterXPath('//span[contains(text(), "price balance US")]')->nextAll('.value')->text();
+    $VIN = $description->filterXPath('//div[contains(text(), "VIN")]')->nextAll('.value')->text();
+
+    $imageLinkSelector = '.listing-detail-gallery .right-images div:nth-child(2) > a';
+    $imageLink = $crawler->filter($imageLinkSelector);
+    if ($imageLink->text('') != '') {
+      $imageLink = $imageLink->attr('href');
+    }
+    else {
+      $imageLink = '';
+    }
+    echo $imageLink;
+
+    $carInfo = new CarInfo($brand, $model, $year, $color, $mileage, $price, $VIN, $imageLink, $link);
+
+    $carInfos[] = $carInfo;
+  }
 }
 else
 {
